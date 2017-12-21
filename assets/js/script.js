5baf66fdd721a122e0a0485cbf1db7307f75ff8b
Dominic Elm <dominic@thoughtram.io>
2017-12-21 11:24:12 +0100

    feat: make price and discount dynamic too

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
+var regularPrice = 299;
+
 var periods = [
-  new Date(2017, 11, 31), // End Q4 2017
-  new Date(2018, 2, 31),  // End Q1 2018
-  new Date(2018, 5, 30),  // End Q2 2018
-  new Date(2018, 8, 30),  // End Q3 2018
-  new Date(2018, 9, 1)    // Regular price beginning Q4
+  { date: new Date(2017, 11, 31), price: 89 },            // End Q4 2017
+  { date: new Date(2018, 2, 31), price: 149 },            // End Q1 2018
+  { date: new Date(2018, 5, 30), price: 199 },            // End Q2 2018
+  { date: new Date(2018, 8, 30), price: 249 },            // End Q3 2018
+  { date: new Date(2018, 9, 1), price: regularPrice }     // Regular price beginning Q4
 ]
 
 $(document).ready(function () {
@@ -33,22 +35,34 @@ function setRemainingDays() {
   today = new Date();
   $daysElement = $('#days-left');
   $daysContainer = $('.days');
+  $price = $('.plan-price');
+  $discount = $('.plan-discount');
+  $regularPrice = $('.regular-price');
 
   var currentPeriod = getCurrentPeriod();
+  var price = currentPeriod['price'];
+  var discount = Math.floor(100 - price * 100 / regularPrice);
   var day = 1000 * 60 * 60 * 24;
-  var daysLeft = Math.ceil((currentPeriod.getTime() - today.getTime()) / (day));
+  var daysLeft = Math.ceil((currentPeriod['date'].getTime() - today.getTime()) / (day));
 
   if (daysLeft <= 7) {
     $daysContainer.addClass('danger');
   }
 
+  if (price === regularPrice) {
+    $regularPrice.addClass('no-currency');
+    $regularPrice.html('Regular Price');
+  }
+
   $daysElement.html(daysLeft);
+  $price.html(price);
+  $discount.html(discount);
 }
 
 function getCurrentPeriod() {
   var sortedPeriods = periods.sort(function (a, b) {
-    var distanceA = Math.abs(today - a);
-    var distanceB = Math.abs(today - b);
+    var distanceA = Math.abs(today - a.date);
+    var distanceB = Math.abs(today - b.date);
     return distanceA - distanceB;
   });
 
